ff89e6d4a4d80de2dd31b708f1bdca090c2ef3c4
Travis Miller <travisbmiller@outlook.com>
2014-10-12 21:44:12 +0200

    update

--- phoneapp.js
+++ phoneapp.js
@@ -82,12 +82,12 @@ $("li").click(function() {
 
   $('a').click(function() {
     $('#employee_panel').css({ "left": "-100%"});
-    $('#employee_dashboard_panel').css({ "left": "0%","transition":"0.1s ease"});
+    // $('#employee_dashboard_panel').css({ "left": "0%","transition":"0.1s ease"});
   });
 
   $('#employee_dashboard_panel').click(function() {
     $('#employee_panel').css({ "left": "0%"});
-    $('#employee_dashboard_panel').css({ "left": "100%","transition":"1s ease"});
+    // $('#employee_dashboard_panel').css({ "left": "100%","transition":"1s ease"});
   });
 
 });
