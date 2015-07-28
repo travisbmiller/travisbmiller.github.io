915251aa22780e6cb7762349620950bc4484c4c1
Travis Miller <travisbmiller@outlook.com>
2015-07-28 01:34:49 -0500

    added social icons for overlay

--- comingsoon/app.js
+++ comingsoon/app.js
@@ -6,12 +6,16 @@ $(".menu").on('click', function () {
             $(".non-drop-down").toggle();
             $(".non-drop-down").toggleClass('fadeInUp');
             $('body').css("overflow", "hidden")
+            $(".social_icon").show()
+            $(".social_icon").toggleClass("fadeInUp")
         });
     } else {
         $("#overlay").fadeToggle()
         $(".non-drop-down").toggle();
         $(".non-drop-down").toggleClass('fadeInUp');
         $('body').css("overflow", "")
+        $(".social_icon").hide()
+        $(".social_icon").toggleClass("fadeInUp")
     }
     
 
