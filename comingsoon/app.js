9b4c38807f4bbd77ed603dec1750b50d75206f87
Travis Miller <travisbmiller@outlook.com>
2015-07-28 21:16:06 -0500

    add blog

--- comingsoon/app.js
+++ comingsoon/app.js
@@ -1,23 +1,26 @@
-$(".menu").on('click', function () {
-    $("[class^='icon_bar']").toggleClass("clicked")
+function showHideNav () {
+    $("[class^='icon_bar']").toggleClass("clicked");
     
     if ( $('.icon_bar_1').hasClass("clicked") ) {
             $("#overlay").fadeToggle(function () {
-            $(".non-drop-down").toggle();
-            $(".non-drop-down").toggleClass('fadeInUp');
-            $('body').css("overflow", "hidden")
-            $(".social_icon").show()
-            $(".social_icon").toggleClass("fadeInUp")
-        });
+                $(".non-drop-down").toggle();
+                $(".non-drop-down").toggleClass('fadeInUp');
+                $('body').css("overflow", "hidden");
+                $(".social_icon").show();
+                $(".social_icon").toggleClass("fadeInUp");
+            });
     } else {
-        $("#overlay").fadeToggle()
+        $("#overlay").fadeToggle();
         $(".non-drop-down").toggle();
         $(".non-drop-down").toggleClass('fadeInUp');
-        $('body').css("overflow", "")
-        $(".social_icon").hide()
-        $(".social_icon").toggleClass("fadeInUp")
+        $('body').css("overflow", "");
+        $(".social_icon").hide();
+        $(".social_icon").toggleClass("fadeInUp");
     }
-    
+}
 
-    
+$(function() {
+    $(".menu").on('click', showHideNav);
 })
+
+
